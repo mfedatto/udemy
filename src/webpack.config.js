1effa78ae132adc72727eff64109b50c9cea0c48
Maurício Fedatto <mfedatto@gmail.com>
2022-10-26 18:34:49 -0300

    section 4; lesson 17; handling sass;

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -29,6 +29,14 @@ module.exports = {
                     "style-loader",
                     "css-loader"
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
         ]
     }
