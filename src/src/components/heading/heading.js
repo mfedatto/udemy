52e4ef0c1cffd54e771d99230bd10c87cf5f5af4
Maurício Fedatto <mfedatto@gmail.com>
2022-10-29 14:11:48 -0300

    section 5; lesson 22; extracting css into separate bundle, part 2;

---
+++ src/src/components/heading/heading.js
@@ -0,0 +1,14 @@
+import "./heading.scss";
+
+class Heading {
+    render() {
+        const body = document.querySelector("body");
+        const h1 = document.createElement("h1");
+
+        h1.innerHTML = "Webpack is awesome";
+
+        body.appendChild(h1);
+    }
+}
+
+export default Heading;
--- src/src/index.js
+++ src/src/index.js
@@ -1,5 +1,8 @@
+import Heading from './components/heading/heading.js';
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 
+const heading = new Heading();
 const helloWorldButton = new HelloWorldButton();
 
+heading.render();
 helloWorldButton.render();
