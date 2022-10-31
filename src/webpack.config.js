70948cf3e78dad36070cf149c67f14f2656f1b34
Maurício Fedatto <mfedatto@gmail.com>
2022-10-31 05:46:09 -0300

    section 5; lesson 26; customizing generated html files;

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.[contenthash].js",
         path: path.resolve(__dirname, "./dist"),
-        publicPath: "dist/",
+        publicPath: "",
         clean: true
     },
     mode: "none",
@@ -58,8 +59,9 @@ module.exports = {
     plugins: [
         new TerserPlugin(),
         new MiniCssExtractPlugin({
-            filename: "styles.css"
+            filename: "styles.[contenthash].css"
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
     ]
 };
